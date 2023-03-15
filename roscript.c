09cba9ddc72d61d7dd856fdffea0c54c03fdb8b2
Rohan Akki <roha.akki@gmail.com>
2023-03-15 04:30:00 -0400

    ok hello world

---
+++ roscript.c
@@ -0,0 +1,13 @@
+//basic c code
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[])
+{
+    //print hello world
+    printf("Hello World");
+    return 0;
+}

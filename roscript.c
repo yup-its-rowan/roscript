f7b1a196152b80984efc9e76750c3ea44b8aa08d
Rohan Akki <roha.akki@gmail.com>
2023-03-15 17:27:19 -0400

    working on sanitiz

--- roscript.c
+++ roscript.c
@@ -8,6 +8,24 @@
 int main(int argc, char const *argv[])
 {
     //print hello world
-    printf("Hello World");
+    if (argc == 1){
+        printf("Add an argument");
+        return 0;
+    }
+
+    int flagPrint = 0;
+    int done = 0;
+    const char* input = NULL;
+    for (int i = 1; i < argc; i++){
+        if (strcmp(argv[i], "-p") == 0){
+            flagPrint = 1;
+        } else if (!done){
+            input = argv[i];
+            done = 1;
+        }
+    }
+    
+
+    printf("%s", input);
     return 0;
 }
